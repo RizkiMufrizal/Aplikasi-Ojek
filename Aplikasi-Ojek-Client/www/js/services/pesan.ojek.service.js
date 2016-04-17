a9d45ce3da53ad015cba1060401a26bdad2b7aa7
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-04-17 16:53:33 +0700

    update pesan ojek service

--- Aplikasi-Ojek-Client/www/js/services/pesan.ojek.service.js
+++ Aplikasi-Ojek-Client/www/js/services/pesan.ojek.service.js
@@ -2,7 +2,8 @@
 angular.module('Aplikasi-Ojek')
   .factory('PesanOjekService', ['$http', function($http) {
 
-    var baseUrl = 'http://127.0.0.1/Aplikasi-Ojek-Server/index.php';
+    var baseUrl = 'http://10.42.0.1/Aplikasi-Ojek-Server/index.php';
+    //var baseUrl = 'http://127.0.0.1/Aplikasi-Ojek-Server/index.php';
 
     return {
       ambilDataPesanan: function() {
