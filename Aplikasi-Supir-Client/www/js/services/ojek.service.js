73ddd5fef5594dae34e7cf2090c7365ae1732c81
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-06-14 07:26:42 +0700

    ganti nama folder

--- Aplikasi-Supir-Client/www/js/services/ojek.service.js
+++ Aplikasi-Supir-Client/www/js/services/ojek.service.js
@@ -2,8 +2,9 @@
 angular.module('Aplikasi-Ojek')
   .factory('OjekService', ['$http', function($http) {
 
-    var baseUrl = 'http://10.42.0.1/Aplikasi-Ojek-Server/index.php';
-    //var baseUrl = 'http://127.0.0.1/Aplikasi-Ojek-Server/index.php';
+    //var baseUrl = 'http://10.42.0.1/Aplikasi-Ojek-Server/index.php';
+    var baseUrl = 'http://127.0.0.1/Aplikasi-Ojek-Server/index.php';
+	//var baseUrl = 'http://ojek-mufrizal.rhcloud.com';
 
     return {
       register: function(ojek) {
